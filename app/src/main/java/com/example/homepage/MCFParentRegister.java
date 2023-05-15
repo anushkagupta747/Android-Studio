7987382b2ed35ec64bbe22f59064f6eb7cc41237
glytch3 <mohammed.samad@learner.manipal.edu>
2023-05-16 03:32:13 +0530

    parent registration done [need to add validations]

--- app/src/main/java/com/example/homepage/MCFParentRegister.java
+++ app/src/main/java/com/example/homepage/MCFParentRegister.java
@@ -2,5 +2,48 @@ package com.example.homepage;
 
 public class MCFParentRegister {
 
+    String parent_name,email,phone,password;
 
+    public MCFParentRegister(){
+
+    }
+
+    public MCFParentRegister(String parent_name, String email, String phone, String password) {
+        this.parent_name = parent_name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getParent_name() {
+        return parent_name;
+    }
+
+    public void setParent_name(String parent_name) {
+        this.parent_name = parent_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- app/src/main/java/com/example/homepage/ParentRegister.java
+++ app/src/main/java/com/example/homepage/ParentRegister.java
@@ -1,15 +1,30 @@
 package com.example.homepage;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.ktx.Firebase;
 
 public class ParentRegister extends AppCompatActivity {
 
+    FirebaseFirestore db;
     private EditText fullNameEditText;
     private EditText emailAddressEditText;
     private EditText phoneEditText;
@@ -21,6 +36,7 @@ public class ParentRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_register);
+        db = FirebaseFirestore.getInstance();
 
         // Initialize EditText fields
         fullNameEditText = findViewById(R.id.parent_fullname_edit);
@@ -34,7 +50,56 @@ public class ParentRegister extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("111111", "onClick: ");
+                // Retrieve the values from EditText fields and store them in strings
+                String fullName = fullNameEditText.getText().toString();
+                String emailAddress = emailAddressEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String retypePassword = retypePasswordEditText.getText().toString();
                 // Perform sign up logic
+                // Query to check if the email address already exists in the 'parent_details' collection
+                db.collection("parent_details")
+                        .whereEqualTo("email", emailAddress)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    QuerySnapshot querySnapshot = task.getResult();
+                                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                                        // Email address already exists in the database
+                                        Log.d("parent add error", "Email address already exists");
+                                        Toast.makeText(ParentRegister.this, "Email address already exists", Toast.LENGTH_SHORT).show();
+                                        // Handle the error or display a message to the user
+                                    } else {
+                                        // Email address doesn't exist, proceed with adding the entry to the database
+                                        MCFParentRegister data = new MCFParentRegister(fullName, emailAddress, phone, password);
+                                        db.collection("parent_details")
+                                                .add(data)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        Log.d("new parent added", "Parent added successfully");
+                                                        Toast.makeText(ParentRegister.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(ParentRegister.this, ParentLogin.class);
+                                                        startActivity(intent);
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.d("parent add error", "Failed to add parent: " + e.getMessage());
+                                                        // Handle the error or display a message to the user
+                                                    }
+                                                });
+                                    }
+                                } else {
+                                    Log.d("parent add error", "Failed to query parent collection: " + task.getException());
+                                    // Handle the error or display a message to the user
+                                }
+                            }
+                        });
             }
         });
 
